4a8bcc69752a289878b419b55f5900f556e8feea
Nosakhare Edokpayi <co.nosakhare@gmail.com>
2021-04-15 21:10:15 +0100

    created budget class and defined basic class methods

---
+++ codesTests.py
@@ -0,0 +1,52 @@
+categories = {
+    "1": {
+        "name": "Food",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "2": {
+        "name": "Clothing",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "3": {
+        "name": "Gas & Transportation",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "4": {
+        "name": "Data & Airtime",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "5": {
+        "name": "Groceries & Utilities",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "6": {
+        "name": "Insurance",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "7": {
+        "name": "Emergencies",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+    "8": {
+        "name": "Savings",
+        "balance": 1122334,
+        "limit": 100
+    },
+
+}
+
+
